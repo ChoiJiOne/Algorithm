f6e852b080537d5272eb84d4c046d391fdca4b81
ChoiJiOne <54870255+ChoiJiOne@users.noreply.github.com>
2021-02-07 10:08:29 +0900

    Change Directory

---
+++ Sorting/Problem/2750_수_정렬하기/2750.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 1000;
+int arr[MAX], N;
+
+void Sort(void)
+{
+	for (int i = N - 1; i > 0; --i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+
+		}
+	}
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+
+
+	return 0;
+}
